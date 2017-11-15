06c7a8a352a0f878065c41cf7e0cc5cb80101d55
MEXEO\s2459247 <jrojasr@scotiabank.com.mx>
2017-11-15 11:50:35 -0600

    Estructura basica de sistema de logs con SLF y Log4J

--- src/main/java/log/Inicio.java
+++ src/main/java/log/Inicio.java
@@ -8,12 +8,12 @@ public class Inicio {
 	public static void main(String[] args) {
 
 		Logger logger = LoggerFactory.getLogger(Inicio.class);
-	    logger.info("Example for SLF$J");
-	    logger.trace("Example for SLF$J");
-		logger.debug("Example for SLF$J");
-	    logger.info("Example for SLF$J");
-	    logger.warn("Example for SLF$J");
-	    logger.error("Example for SLF$J");
+	    logger.info("Example for SLF4J");
+	    logger.trace("Example for SLF4J");
+		logger.debug("Example for SLF4J");
+	    logger.info("Example for SLF4J");
+	    logger.warn("Example for SLF4J");
+	    logger.error("Example for SLF4J");
 
 	}
 
